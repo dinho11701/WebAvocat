31ec3ef11b608ae5b61247af5a1a8bcea44ca25f
dinho11701 <dinho11701@Virtual.myguest.virtualbox.org>
2024-03-10 17:39:53 -0400

    ajout modif port pour bon fonctionnement

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ app.post('/send-email', upload.single('attachment'), (req, res) => {
 
 
 
-const PORT = process.env.PORT || 300;
+const PORT = process.env.PORT || 3007;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
